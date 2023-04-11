f67c28ab15059db081969852f3797448291138d2
AHachey17 <ahachey17@gmail.com>
2023-04-10 21:21:46 -0400

    credits

--- Day21SortArrayStringLength.js
+++ Day21SortArrayStringLength.js
@@ -11,6 +11,9 @@ Your function would return the following array:
 All of the strings in the array passed to your function will be different lengths, so you will not have to decide how to order multiple strings of the same length.
 */
 
+//Learned that sort is a loop and a and b are the 2 intial elements of the array being looped through the function(a-b), which sorts array in ascending order - Erica
+
+//Also worked on with Joyce and Cindy
 
 
 
